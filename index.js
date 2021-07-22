4d7be5fbb1c29bb485f9b5576479b1f11c2663a2
ashutosh <ashutoshyadav699@gmail.com>
2021-07-23 02:46:04 +0530

    Added private messaging

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('express')();
+let ejs = require('ejs');
+const bodyParser = require("body-parser");
 var express = require('express');
 const http = require('http').Server(app);
+app.set('view engine', 'ejs');
 
 const io = require("socket.io")(http, {
     cors: {
@@ -8,19 +11,32 @@ const io = require("socket.io")(http, {
       methods: ["GET", "POST"]
     }
   });
-
+let userList =[];
+let users=0;
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index' , {users : users , userList: userList} );
 });
-let user;
+
 io.on('connection', (socket) => {
+  // socket.to(socket.id).emit("inilist", { userList : userList});
+  users=users+1;
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-gone');
+    users=users-1;
   });
+  socket.on("private message", (data) => {
+    socket.to(data.to).emit("private message", {
+      msg: data.message ,
+      from: data.from,
+      to : data.to,
+    });
+  });
+
   socket.on('user-connected' , (userName) =>{
-    socket.broadcast.emit('show-new' , userName);
+    userList.push({name : userName , _id : socket.id })
+    socket.broadcast.emit('show-new' , {user : userName , users : users , userList: userList});
   });
   socket.on('message' , (msg)=>{
      socket.broadcast.emit('recieved', msg );
--- public/client.js
+++ public/client.js
@@ -1,20 +1,19 @@
 
 let socket = io();
-
-  
 const form = document.getElementsByClassName("form");
-
 const messageInput = document.querySelector('#input');
 const MymessageContainer = document.querySelector('#bubbles');
 const notifcations = document.getElementById('#typing');
-
-
+const chatWindow = document.getElementById('chatWindow');
+const listID = document.getElementById('listID')
 let userName;
 
+let olduserlist = [];
 do{
 userName = prompt("Enter your name to join");
 }while(!userName);
 socket.emit('user-connected' , userName);
+let lastUser = userName;
 
 socket.on('user-gone', function() {
 
@@ -26,54 +25,103 @@ socket.on('user-gone', function() {
     messageDiv.innerHTML = markup;
     MymessageContainer.appendChild(messageDiv);
     scrollTop();
-
 });
-socket.on('show-new' , (user)=>{
-    console.log("no defer");
+
+function listSetter (newuserlist) {
+    newuserlist.forEach((ele)=>{
+        if(ele.name != userName){
+       let namediv = document.createElement('button');
+       namediv.type = "button"
+       namediv.classList.add('list')
+       let privateInput = document.createElement("input");
+       privateInput.classList.add('person');
+       privateInput.placeholder ="Send a private message";
+       privateInput.id = ele._id ;
+       privateInput.name = ele.name ;
+       let markup = `<b>${ele.name}</b><br>`
+       namediv.innerHTML = markup;
+       namediv.appendChild(privateInput)
+       listID.appendChild(namediv);
+    }
+    })
+}
+socket.on('show-new' , (data)=>{
+    let newuserlist = data.userList;
+    let finalList = [];
+    let  flag =0;
+    for(var i=0; i< newuserlist.length; i++) {
+        if(flag==1){
+            finalList.push(newuserlist[i]);
+            lastUser = newuserlist[i].name;
+        }
+        if(newuserlist[i].name === lastUser ) {
+            flag=1;
+        }   
+    } 
+    
+    listSetter(finalList);
+    addListener();
+    Naam = data.user
+    numb = data.users
+    
     let messageDiv= document.createElement('div');
     messageDiv.classList.add('bubble' , 'bubble-wrap');
-
+    
     let markup = `
-
-    <b>${user}</b>
-    <b>Joined the chat!</b>
+    <b>${Naam}</b>
+    <p>Joined the chat! We now have ${numb} people In the room</p>
     `
     messageDiv.innerHTML = markup;
     MymessageContainer.appendChild(messageDiv);
     scrollTop();
 });
 socket.on('recieved', function(msg) {
-
     appendMessage(msg , 'left');
     scrollTop();
 });
-// socket.on('user-typing' , function(user){
-//     notifcations.innerHTML = 'is typing...';
-// });
 
 messageInput.addEventListener('keyup' , (e) =>{
-   console.log(e.keyCode);
+//    console.log(e.keyCode);
     if(e.key === 'Enter' || e.keyCode === 190){
-      sendMessage(e.target.value , 'You');
+      sendMessage(e.target.value , false , {});
        e.target.value = '';
   }
 })
-
-function sendMessage(msg , str){
+    
+socket.on("private message", function(data) {
+    let mesg2 ={
+        user: data.from+' sent you privately' ,
+        message: data.msg.message
+    } 
+    appendMessage(mesg2 , 'left');
+    scrollTop(); 
+ });
+function sendMessage(msg , isPersonal , personData ){
  let mesg = {
      user: userName,
      message: msg
  }
- let mesg2 ={
-     user: str,
-     message: msg
+ 
+ if(isPersonal === true){
+    let mesg2 ={
+        user: 'You sent to '+personData.name+' privately' ,
+        message: msg
+    }
+    appendMessage(mesg2 , 'right');
+       socket.emit("private message", { message: mesg, to: personData._id , from: userName });
  }
- appendMessage(mesg2 , 'right');
- socket.emit('message' , mesg);
+ else{
+    let mesg2 ={
+        user: 'You' ,
+        message: msg
+    }
+    appendMessage(mesg2 , 'right');
+    socket.emit('message' , mesg);
+ }
  scrollTop();
 }
 
-function  appendMessage(msg , type){
+function  appendMessage(msg , type ){
     let messageDiv= document.createElement('div');
     let className = type ;
     messageDiv.classList.add(className , 'bubble' , 'bubble-wrap');
@@ -90,6 +138,32 @@ function  appendMessage(msg , type){
 }
 function scrollTop(){
     MymessageContainer.scrollTop = MymessageContainer.scrollHeight; 
-    console.log('scrolling!');
+}
+let checkArray = []
+ function addListener () { 
+    const personArray = document.getElementsByClassName('person');
+    [...personArray].forEach( (elem) => {
+        if(checkArray.includes(elem.getAttribute("id")) === true){
+              return;
+        }
+        console.log("Adding listeners:)")
+        console.log(elem.getAttribute("id"))
+        
+          elem.addEventListener('keyup', (e) => {
+            let personData = {
+               name:  e.target.getAttribute("name") ,
+               _id : e.target.getAttribute("id")
+            }
+            if(e.key === 'Enter' || e.keyCode === 190){
+                sendMessage(e.target.value , true , personData );
+                 e.target.value = '';
+            }
+            
+        }); 
+        console.log(checkArray.includes(elem.getAttribute("id")))
+        checkArray.push(elem.getAttribute("id"));
+        
+    });
 }
 
+window.onload = addListener();
